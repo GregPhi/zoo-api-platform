3f656bf436a92dccf442ae3b2ed65207669ffbf8
GregPhi <gregoire.philippot@laposte.net>
2021-02-23 16:30:16 +0100

    fix project

--- zoo-api/migrations/Version20210223144411.php
+++ zoo-api/migrations/Version20210223144411.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210220143910 extends AbstractMigration
+final class Version20210223144411 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20210220143910 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE animal (id INT AUTO_INCREMENT NOT NULL, animal_position_id INT DEFAULT NULL, animal_name VARCHAR(255) NOT NULL, animal_description VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_6AAB231F7A3EFDFF (animal_position_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE animal (id INT AUTO_INCREMENT NOT NULL, animal_zooposition_id INT DEFAULT NULL, animal_name VARCHAR(255) NOT NULL, animal_description VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_6AAB231F678B7AD7 (animal_zooposition_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE answer (id INT AUTO_INCREMENT NOT NULL, question_id INT DEFAULT NULL, answer VARCHAR(255) NOT NULL, is_good TINYINT(1) NOT NULL, INDEX IDX_DADD4A251E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, quiz_id INT DEFAULT NULL, question VARCHAR(255) NOT NULL, good_answer TINYINT(1) NOT NULL, INDEX IDX_B6F7494E853CD175 (quiz_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE quiz (id INT AUTO_INCREMENT NOT NULL, quiz_name VARCHAR(255) NOT NULL, best_score INT NOT NULL, is_make TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE zoo_position (id INT AUTO_INCREMENT NOT NULL, longitude INT NOT NULL, lagitude INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE animal ADD CONSTRAINT FK_6AAB231F7A3EFDFF FOREIGN KEY (animal_position_id) REFERENCES zoo_position (id)');
+        $this->addSql('ALTER TABLE animal ADD CONSTRAINT FK_6AAB231F678B7AD7 FOREIGN KEY (animal_zooposition_id) REFERENCES zoo_position (id)');
         $this->addSql('ALTER TABLE answer ADD CONSTRAINT FK_DADD4A251E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
         $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494E853CD175 FOREIGN KEY (quiz_id) REFERENCES quiz (id)');
     }
@@ -35,7 +35,7 @@ final class Version20210220143910 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A251E27F6BF');
         $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494E853CD175');
-        $this->addSql('ALTER TABLE animal DROP FOREIGN KEY FK_6AAB231F7A3EFDFF');
+        $this->addSql('ALTER TABLE animal DROP FOREIGN KEY FK_6AAB231F678B7AD7');
         $this->addSql('DROP TABLE animal');
         $this->addSql('DROP TABLE answer');
         $this->addSql('DROP TABLE question');
